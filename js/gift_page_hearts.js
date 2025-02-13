5bc0bc93d59d10c97d5de4b75f24378040adcc33
Dmytro Korsun <dmitry.korsun.v@gmail.com>
2025-02-13 10:42:14 +0200

    Gift_page v1

---
+++ js/gift_page_hearts.js
@@ -0,0 +1,13 @@
+function createHearts() {
+    for (let i = 0; i < 15; i++) {
+        let heart = document.createElement("div");
+        heart.classList.add("heart");
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = Math.random() * 2 + 2 + "s";
+        document.body.appendChild(heart);
+        setTimeout(() => {
+            heart.remove();
+        }, 3000);
+    }
+}
+setInterval(createHearts, 500);
