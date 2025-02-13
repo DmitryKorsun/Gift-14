2239c2b34bd09f0b74c72019555862a69fb43e8c
Dmytro Korsun <dmitry.korsun.v@gmail.com>
2025-02-13 11:20:34 +0200

    Changes in some points

--- js/gift_page_hearts.js
+++ js/gift_page_hearts.js
@@ -2,7 +2,7 @@ function createHearts() {
     for (let i = 0; i < 15; i++) {
         let heart = document.createElement("div");
         heart.classList.add("heart");
-        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.right = Math.random() * 100 + "vw";
         heart.style.animationDuration = Math.random() * 2 + 2 + "s";
         document.body.appendChild(heart);
         setTimeout(() => {
